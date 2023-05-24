81f9dcf1f82bce43920aa91cc174ff6ddfe89247
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-24 02:51:45 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""Adetunji Olasubomi
 """
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -21,14 +21,14 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
+    """
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page.
+    """
     """
     return render_template('2-index.html')
 
