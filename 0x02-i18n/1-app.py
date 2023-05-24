c3d3ddb8365ae736da6a9dee58b52407d0f38f4d
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-24 02:51:08 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""Adetunji Olasubomi
 """
 from flask_babel import Babel
 from flask import Flask, render_template
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -21,7 +21,7 @@ babel = Babel(app)
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page.
+    """
     """
     return render_template('1-index.html')
 
