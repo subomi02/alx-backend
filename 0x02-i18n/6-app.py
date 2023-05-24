fd9d651a468a653b4bc3faa7a913b7ca9ac1e9b1
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-24 02:54:16 +0100

    Update 6-app.py

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""Adetunji Olasubomi
 """
 from flask_babel import Babel
 from typing import Union, Dict
@@ -7,7 +7,7 @@ from flask import Flask, render_template, request, g
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -27,7 +27,7 @@ users = {
 
 
 def get_user() -> Union[Dict, None]:
-    """Retrieves a user based on a user id.
+    """
     """
     login_id = request.args.get('login_as', '')
     if login_id:
@@ -37,7 +37,7 @@ def get_user() -> Union[Dict, None]:
 
 @app.before_request
 def before_request() -> None:
-    """Performs some routines before each request's resolution.
+    """
     """
     user = get_user()
     g.user = user
@@ -45,7 +45,7 @@ def before_request() -> None:
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
+    """
     """
     locale = request.args.get('locale', '')
     if locale in app.config["LANGUAGES"]:
@@ -60,7 +60,7 @@ def get_locale() -> str:
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page.
+    """
     """
     return render_template('6-index.html')
 
