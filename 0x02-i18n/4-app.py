4efa7a4a8f0785f790a2feb2256d21abe630ff51
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-24 02:52:22 +0100

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""Adetunji Olasubomi
 """
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -21,7 +21,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
+    """
     """
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
@@ -36,7 +36,7 @@ def get_locale() -> str:
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page.
+    """
     """
     return render_template('4-index.html')
 
